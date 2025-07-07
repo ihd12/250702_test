53e9eeeaaf7bda44b47e083e76c7846881dae379
DESKTOP-GN39FRF\it <hyedonglim@gmail.com>
2025-07-07 09:15:57 +0900

    board에서 수정

--- test/src/main/java/org/zerock/test/board/controller/BoardController.java
+++ test/src/main/java/org/zerock/test/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package org.zerock.test.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// board에서 수정
 @Controller
 @RequestMapping()
 public class BoardController {
